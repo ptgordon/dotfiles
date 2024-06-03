83b80bf4b5ac6ed0c9e2dfc71ef939d2700ab610
Patrick Gordon <pgordon8001@gmail.com>
2024-06-02 22:49:00 -0500

    added nvim initial commits

---
+++ nvim/init.lua
@@ -0,0 +1,32 @@
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({
+	{"rafi/awesome-vim-colorschemes"}
+})
+
+vim.o.number = true         -- Enable line numbers
+vim.o.tabstop = 4           -- Number of spaces a tab represents
+vim.o.shiftwidth = 4        -- Number of spaces for each indentation
+vim.o.expandtab = true      -- Convert tabs to spaces
+vim.o.smartindent = true    -- Automatically indent new lines
+vim.o.wrap = false          -- Disable line wrapping
+vim.o.cursorline = true     -- Highlight the current line
+vim.o.termguicolors = true  -- Enable 24-bit RGB colors
+vim.cmd.colorscheme('gruvbox')  -- Set colorscheme
+
+-- Syntax highlighting and filetype plugins
+vim.cmd('syntax enable')
+vim.cmd('filetype plugin indent on')
+
+
