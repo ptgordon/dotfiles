5f97df7211dc5d88553b40be75b22455cde662d1
Patrick Gordon <patrick.gordon@milwaukeetool.com>
2025-10-29 09:46:43 -0500

    added text files to the spellcheck list

--- nvim/init.lua
+++ nvim/init.lua
@@ -148,7 +148,7 @@ lspconfig.lua_ls.setup {
 }
 
 vim.api.nvim_create_autocmd("FileType", {
-  pattern = { "tex", "plaintex", "markdown" },
+  pattern = { "tex", "plaintex", "markdown", "txt" },
   callback = function()
     vim.opt_local.spell = true
     vim.opt_local.spelllang = { "en_us" }
