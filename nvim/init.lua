0a9170327199745826f670772c8c4b511ca3c3ca
Patrick Gordon <pgordon8001@gmail.com>
2025-11-11 22:37:54 -0600

    fixed autocomplete to allow for control p and n

--- nvim/init.lua
+++ nvim/init.lua
@@ -214,6 +214,26 @@ cmp.setup({
       end
     end, { "i", "s" }),
 
+    ["<C-n>"] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_next_item()
+      elseif luasnip.locally_jumpable(1) then
+        luasnip.jump(1)
+      else
+        fallback()
+      end
+    end, { "i", "s" }),
+
+    ["<C-p>"] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_prev_item()
+      elseif luasnip.locally_jumpable(-1) then
+        luasnip.jump(-1)
+      else
+        fallback()
+      end
+    end, { "i", "s" }),
+
     },
 
     formatting = {
